10aec954296e298443a5fda4b974280d09721ec5
Bojumbo <bogdanbrodiak@gmail.com>
2026-02-28 12:27:58 +0200

    button

--- plugin.js
+++ plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Власний Сервер
- * @version 1.0.1
- * @description Мій плагін для перегляду фільмів
+ * @version 1.0.3
+ * @description Виправлено для нового лейауту Lampa
  * @author Bogdan
  */
 
@@ -14,38 +14,28 @@
     var backend_url = 'https://tv.bojumbohost.pp.ua';
 
     function myPlugin() {
-        console.log('Plugin: Мій Сервер - запуск логіки');
-
-        // Повідомлення при старті (щоб ти бачив, що він працює)
-        setTimeout(function () {
-            Lampa.Noty.show('Плагін "Власний Сервер" активний!');
-        }, 3000);
-
-        // Слухаємо подію завантаження картки фільму
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
-                console.log('Plugin: Картка фільму завантажена', e);
-
-                // Спроба знайти контейнер для кнопок декількома методами
                 var render = e.object.activity.render();
-                var container = render.find('.full-start__buttons');
 
-                // Якщо стандартний шлях не спрацював, шукаємо будь-які кнопки на екрані
+                // ШУКАЄМО ТВОЮ НОВУ ПАНЕЛЬ КНОПОК
+                var container = render.find('.full-start-new__buttons');
+
+                // Якщо не знайшли нову, шукаємо старі (про всяк випадок)
                 if (container.length === 0) {
-                    container = render.find('.full-buttons');
+                    container = render.find('.full-start__buttons, .full-buttons');
                 }
 
                 if (container.length > 0) {
-                    // Перевіряємо, чи ми вже не додали кнопку раніше
                     if (container.find('.my-server-btn').length > 0) return;
 
-                    // Створюємо яскраву кнопку
-                    var btn = $('<div class="full-start__button selector view--btn my-server-btn" style="background-color: #e50914 !important; color: #fff !important; margin-top: 10px; border: 2px solid white;">🔴 МІЙ СЕРВЕР</div>');
+                    // Створюємо кнопку (зробив її в стилі Lampa, але з червоним акцентом)
+                    var btn = $('<div class="full-start__button selector view--btn my-server-btn" style="background-color: #e50914 !important; color: #fff !important;">Мій Сервер</div>');
 
                     btn.on('hover:enter hover:click hover:touch', function () {
                         Lampa.Loading.start();
 
-                        var movie_data = e.data || e.object.data;
+                        var movie_data = e.object.data || e.data;
                         var title = movie_data.title || movie_data.name;
                         var api_url = backend_url + '/api/stream?id=' + movie_data.id + '&title=' + encodeURIComponent(title);
 
@@ -67,12 +57,11 @@
                         });
                     });
 
-                    // Додаємо в самий початок списку кнопок
-                    container.prepend(btn);
-                    console.log('Plugin: Кнопка успішно додана');
-                } else {
-                    console.error('Plugin: Не вдалося знайти блок .full-start__buttons');
-                    Lampa.Noty.show('Помилка: не знайдено блок кнопок');
+                    // Додаємо кнопку в кінець списку кнопок
+                    container.append(btn);
+
+                    // Обов'язково змушуємо Lampa перерахувати навігацію пультом/клавіатурою
+                    Lampa.Controller.enable('full');
                 }
             }
         });
